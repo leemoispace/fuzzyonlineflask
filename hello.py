3ac9b22334dd7ccbc74fc3cfa3b3a3172e7d469f
leemoispace <leemoispace@gmail.com>
2019-03-29 21:18:06 +0800

    第二章
    第二章done，第三章模板

--- hello.py
+++ hello.py
@@ -1,13 +1,26 @@
 from flask import Flask
+from flask import request
+from flask import redirect
+
 app = Flask(__name__)
 # Flask 用这个参数确定应用的位置，进而找到应
 # 用中其他文件的位置，例如图像和模板。
 
-@app.route('/')
+#@app.route('/')
 # 定义路由的最简便方式，是使用应用实例提供的app.route 装饰器
-def index():
-    return '<h1>Hello World!</h1>'
+#def index():
+#    return '<h1>Hello World!</h1>'
 
-@app.route('/user/<name>')
+@app.route('/user/<name>')#动态路由
 def user(name):
     return '<h1>Hello, {}!</h1>'.format(name)
+
+@app.route('/browser')
+def browser():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is {}</p>'.format(user_agent)
+
+@app.route('/')
+def index():
+    return redirect('http://youtube.com')
+
